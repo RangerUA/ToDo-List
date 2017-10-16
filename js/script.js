8659ddc21001954dffce728f0f003dac9a4f7695
RangerUA <ramzess.zvir@gmail.com>
2017-10-16 05:12:14 +0300

    Implemented the task deletion

--- js/script.js
+++ js/script.js
@@ -7,13 +7,19 @@ let key = 'myTask';
 let tasks = [];
 if(localStorage.getItem(key) !== null){
     tasks = JSON.parse(localStorage.getItem(key));
-    renderTask();
+    console.log(tasks);
+    createTask();
 }
 
 addTask.onclick = function () {
-    writeToLocalStorage();
-    renderTask();
-    inputTask.value = '';
+    if(inputTask.value){
+        writeToLocalStorage();
+        createTask();
+        inputTask.value = '';
+        inputTask.placeholder = 'Start typing here to create a task...';
+    } else {
+        inputTask.placeholder = "The field can't be empty!";
+    }
 };
 
 function writeToLocalStorage(){
@@ -25,13 +31,43 @@ function writeToLocalStorage(){
     localStorage.setItem(key, JSON.stringify(tasks));
 }
 
-function renderTask(){
-
+function createTask(){
     let out = '';
+    for(let key in tasks){
+        out += `<li class="list-group-item list-group-item-action outTask">${tasks[key].task}</li>`;
+    }
+    taskList.innerHTML = out;
+}
+
+let remove = document.querySelectorAll('.outTask');
+for(let k of remove){
+    console.log(k.innerHTML);
+    k.addEventListener('click', function (e) {
+        console.log(e.target);
+        for(let j in tasks){
+            if(tasks[j].check !== true && k.innerHTML === tasks[j].task){
+                console.dir(j);
+                let a = tasks.splice(j, 1);
+                console.log(a);
+                console.log(tasks);
+                localStorage.setItem(key, JSON.stringify(tasks));
+            }
+        }
+        e.target.remove();
+    })
+}
+
+for(let j in tasks){
+    if(tasks[j].check !== true && j.innerHTML === tasks[j].task){
+        console.log(j);
+    }
+}
+
+function removeTask(){
+    let remove = document.querySelectorAll('.outTask');
     for(let key in tasks){
         if(tasks[key].check !== true){
             out += `<li class="list-group-item list-group-item-action outTask">${tasks[key].task}</li>`;
         }
     }
-    taskList.innerHTML = out;
 }
