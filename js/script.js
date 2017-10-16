334f2d71ac0992a000554bc61d21887533833219
RangerUA <ramzess.zvir@gmail.com>
2017-10-16 03:43:55 +0300

    Implemented the addition of a task

--- js/script.js
+++ js/script.js
@@ -1,4 +1,37 @@
-    let addTask = document.getElementById('addTask');
-    addTask.onclick = function () {
-        alert('Congratulations!')
-    };
+let addTask = document.getElementById('addTask');
+let inputTask = document.getElementById('inputTask');
+let taskList = document.getElementById('taskList');
+
+let key = 'myTask';
+
+let tasks = [];
+if(localStorage.getItem(key) !== null){
+    tasks = JSON.parse(localStorage.getItem(key));
+    renderTask();
+}
+
+addTask.onclick = function () {
+    writeToLocalStorage();
+    renderTask();
+    inputTask.value = '';
+};
+
+function writeToLocalStorage(){
+    let temp = {};
+    temp.task = inputTask.value;
+    temp.check = false;
+    let i = tasks.length;
+    tasks[i] = temp;
+    localStorage.setItem(key, JSON.stringify(tasks));
+}
+
+function renderTask(){
+
+    let out = '';
+    for(let key in tasks){
+        if(tasks[key].check !== true){
+            out += `<li class="list-group-item list-group-item-action outTask">${tasks[key].task}</li>`;
+        }
+    }
+    taskList.innerHTML = out;
+}
